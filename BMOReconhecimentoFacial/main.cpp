26b4490b549468a42314e4b70879003152fbb650
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-04-06 10:19:27 -0300

    Initial Commit

---
+++ BMOReconhecimentoFacial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BMOReconhecimentoFacial
+//
+//  Created by Breno Medeiros on 06/04/20.
+//  Copyright © 2020 ProgramasBMO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
